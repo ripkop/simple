6116132d2de37f401bd3c9dccf94cfee21d3ba38
milad <mm70ir3@gmail.com>
2018-10-03 19:47:13 +0330

    commit

--- simpleapp.py
+++ simpleapp.py
@@ -10,7 +10,7 @@ start = int(round(time.time()))
 @app.route("/")
 def hello_world():
 
-    return "Hello World from Distelli! test tost test"
+    return "Hello World from milad for testing distelli"
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="python simpleapp.py -p <port>")
